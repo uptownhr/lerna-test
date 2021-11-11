4298d2b5be84ffc47af6c8b35e5e49a30da21c28
uptown <uptownhr@gmail.com>
2021-11-11 10:39:50 -0800

    test

--- packages/typescript-package/src/typescript-package.ts
+++ packages/typescript-package/src/typescript-package.ts
@@ -1,6 +1,6 @@
 const printName = (firstName, lastName) => {
     const fullName = firstName + " " + lastName;
-    console.log(fullName, 'test');
+    console.log(fullName, 'test2');
     return fullName;
   }
 module.exports = printName;
